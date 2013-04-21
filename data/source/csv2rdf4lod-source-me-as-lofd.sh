9bf89a4b761e79d4f6e52d2bbf1c91cba59563ba
Tim Lebo <timrdf@gmail.com>
2013-04-21 21:48:36 +0000

    During install: added stub directories and readme files.

---
+++ data/source/csv2rdf4lod-source-me-as-lofd.sh
@@ -0,0 +1,15 @@
+#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;
+#3>     rdfs:seeAlso 
+#3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
+#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
+
+source /home/lofd/prizms/lofd/data/source/csv2rdf4lod-source-me-for-lofd.sh
+source /home/lofd/prizms/lofd/data/source/csv2rdf4lod-source-me-credentials.sh
+export CSV2RDF4LOD_CONVERT_DATA_ROOT="/home/lofd/prizms/lofd/data/source"
+export CSV2RDF4LOD_PUBLISH_VARWWW_DUMP_FILES="true"
+export CSV2RDF4LOD_PUBLISH_VARWWW_ROOT="/var/www"
+export PATH=$PATH`/home/lofd/opt/prizms/bin/install/paths.sh`
+export CLASSPATH=$CLASSPATH`/home/lofd/opt/prizms/bin/install/classpaths.sh`
+export CSV2RDF4LOD_HOME="/home/lofd/opt/prizms/repos/csv2rdf4lod-automation"
+export CSV2RDF4LOD_PUBLISH_VIRTUOSO="true"
+export CSV2RDF4LOD_PUBLISH_SUBSET_SAMPLES="true"
